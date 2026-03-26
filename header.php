c57c2489ddba20fea2752b70a00ece849eafad6e
andrebauru <andretsc@gmail.com>
2026-03-26 09:14:34 +0900

    Release v1.2.2: pacote leve sem MP4/GIF e ajustes de header

--- header.php
+++ header.php
@@ -20,11 +20,13 @@ if (!defined('ABSPATH')) {
 <header class="site-header" role="banner">
     <div class="header-inner">
         <div class="branding" itemscope itemtype="https://schema.org/Organization">
-            <?php
-            if (function_exists('the_custom_logo') && has_custom_logo()) {
-                the_custom_logo();
-            }
-            ?>
+            <div class="site-logo" aria-hidden="true">
+                <?php
+                if (function_exists('the_custom_logo') && has_custom_logo()) {
+                    the_custom_logo();
+                }
+                ?>
+            </div>
             <h1 class="site-title" itemprop="name">
                 <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>
             </h1>
